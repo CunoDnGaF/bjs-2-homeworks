c0f2512791dcd0a85484840546ee2f596e977c58
Nimat13 <nikitamatrosov4@gmail.com>
2023-04-05 14:16:16 +0300

    Четвертое задание готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+  if (this.marks !== undefined) {
+    this.marks.push(...marksToAdd);
+  } 
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks === undefined || this.marks.length === 0) {
+    return 0;
+  } else {
+  let sum = this.marks.reduce(function (x, y) { return x + y; }, 0);
+  let average = sum / (this.marks.length);
+  return average; 
+  }
 }
 
+
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
 }
