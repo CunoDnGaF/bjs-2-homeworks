82702f572eb2857a66825f87acdd22cbf71b5f6c
Nimat13 <nikitamatrosov4@gmail.com>
2023-04-17 14:59:55 +0300

    Третье задание готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,14 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    return (arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]));
+  }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+  function getUsersNamesInAgeRange(users, gender) {
+    let filteredUsers = users.filter(user => user.gender === gender);
+    let agesSum = filteredUsers.map(user => user.age).reduce((acc, val) => acc + val, 0);
+    let averageAge = agesSum / filteredUsers.length;
+    if(Number.isFinite(averageAge)) {
+      return averageAge;
+    } else {
+      return 0;
+    } 
+  }
