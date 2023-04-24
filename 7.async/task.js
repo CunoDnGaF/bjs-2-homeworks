e3c3aae304fa566e927ac55e9d52187c512714c0
Nimat13 <nikitamatrosov4@gmail.com>
2023-04-24 15:03:58 +0300

    Седьмое задание готово

---
+++ 7.async/task.js
@@ -0,0 +1,54 @@
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+    
+    addClock(time, callback) {
+        if (!time || !callback) {
+            throw new Error ('Отсутствуют обязательные аргументы');
+        } else if (this.alarmCollection.find(alarm => alarm.time === time)) {
+            console.warn('Уже присутствует звонок на это же время');
+        }
+        return this.alarmCollection.push({callback, time, canCall : true});
+    }
+
+    removeClock(time) {
+       return this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+    }
+
+    getCurrentFormattedTime() {
+        return new Date().toTimeString().slice(0, 5);
+    }
+
+    start() {
+        if (this.intervalId) {
+          return 
+        } else {
+            this.intervalId = setInterval(() => {
+                let currentTime = this.getCurrentFormattedTime();
+                this.alarmCollection.forEach((alarm) => {
+                    if(alarm.time === currentTime && alarm.canCall) {
+                        alarm.canCall = false;
+                        alarm.callback();
+                    }
+                });
+            }, 1000);
+        }
+
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach((alarm) => alarm.canCall = true);
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
